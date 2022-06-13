26c2c8c1e11b018b1c90c7ce079d31d317aa5cb9
codethefeel <yogeshsolankim76@gmail.com>
2022-06-13 23:32:26 +0530

    initial commit

--- script.js
+++
@@ -1,8 +0,0 @@
- const toggle = document.querySelector('.toggle');
- const navigation = document.querySelector('.navigation');
-
- 
-toggle.addEventListener('click',function(){
-    toggle.classList.toggle('active')
-    navigation.classList.toggle('active')
-})
